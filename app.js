9450279997c61854158788e8005bcafc1fb07ce6
Viktoriya Gridasova <urep@ukr.net>
2016-02-29 05:03:21 +0200

    tableCar

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ var http = require('http');
 
 
 var app = express();
-app.set('port', 8000);
+app.set('port', 5454);
 
 app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-var pgp = require("pg-promise")(/*options*/);
+var pgp = require("pg-promise")();
 var db = pgp("postgres://urep:123@localhost:5432/car_db");
-var carDB;
-//db.one("SELECT array_to_json(array_agg(cars)) FROM cars", 123)
+var carDB = {array_to_json: []};
 db.one("SELECT array_to_json(array_agg(cars)) FROM cars", 123)
   .then(function (data) {
     carDB = data;
-    console.log("DATA:", carDB);
+    console.log("DATA   :", carDB);
   })
   .catch(function (error) {
     console.log("ERROR:", error);
@@ -28,70 +27,43 @@ app.get('/', function(req, res, next) {
 });
 
 var server = http.createServer(app);
-server.listen(app.get('port'), function() {
-  console.log('2222ddssssss');
-});
 
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
-  socket.on('chat message4', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(msg){
+    for (var i = 0; i < carDB.array_to_json.length; i++) {
+      if (carDB.array_to_json[i].id == msg) {
+        carDB.array_to_json.splice(i, 1);
+        db.one("DELETE FROM cars WHERE id = " + msg, 123)
+          .then(function (data) {
+            console.log("DATA :", data);
+          })
+          .catch(function (error) {
+            console.log("ERROR:", error);
+        });
+      }
+    }
   });
-});
-
-/*var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+  socket.on('addCar', function(car){
+    var carId;
+    var id = db.one("INSERT INTO cars (name, capacity) VALUES ('" + car.name + "', " + car.capacity + ") RETURNING id", 123)
+      .then(function (data) {
+        carId = data;
+        console.log(carId);
+        carDB.array_to_json.push({
+          id: carId.id,
+          name: car.name,
+          capacity: car.capacity,
+        });
+        socket.emit('addCar', carId.id);
+      })
+      .catch(function (error) {
+        console.log("ERROR:", error);
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
-*/
+server.listen(app.get('port'), function() {
+});
 
 module.exports = app;
